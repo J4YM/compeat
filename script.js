550249b32b073938785dfbc9fe69733fee970dfd
JayM <80807840+J4YM@users.noreply.github.com>
2023-05-16 21:45:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ function downloaded() {
   Swal.fire({
     icon: 'success',
     title: 'File Downloading!',
-    text:'Please wait for a few moments',
+    text:'Please wait a few seconds',
     footer:'File will be named b07byo.apk'
   })
 }
+
+function downloaded2() {
+  Swal.fire({
+    icon: 'success',
+    title: 'File Downloading!',
+    text:'Please wait a few seconds',
+  })
+}  
 
 const myCarouselElement = document.querySelector('#myCarousel')
 
